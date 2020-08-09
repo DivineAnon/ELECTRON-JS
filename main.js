e0bb8e942963f583849bf4bc504867e4c5bcc0ef
durian-studio2 <2@durian.studio>
2020-08-10 01:41:40 +0700

    updated dashboard imputer

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 const { menu } = require("./menu");
 
-let mainWindow;
+let mainWindow, imputerWindow;
 
 const isWindows = process.platform === "win32";
 
@@ -21,10 +21,29 @@ function createWindow() {
     frame: isWindows ? false : true //Remove frame to hide default menu
   });
 
+  imputerWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js")
+      // (NOT RECOMMENDED)
+      // If true, we can skip attaching functions from ./menu-functions.js to window object in preload.js.
+      // And, instead, we can use electron APIs directly in renderer.js
+      // From Electron v5, nodeIntegration is set to false by default. And it is recommended to use preload.js to get access to only required Node.js apis.
+      // nodeIntegration: true
+    },
+    frame: isWindows ? false : true //Remove frame to hide default menu
+  });
+
   mainWindow.loadFile("index.html");
+  imputerWindow.loadFile("dashboard-imputer.html");
 
   mainWindow.on("closed", function() {
     mainWindow = null;
+  });
+
+  imputerWindow.on("closed", function(){
+    imputerWindow = null;
   });
 }
 
@@ -36,6 +55,7 @@ app.on("window-all-closed", function() {
 
 app.on("activate", function() {
   if (mainWindow === null) createWindow();
+  if (imputerWindow === null) createWindow();
 });
 
 // Register an event listener. When ipcRenderer sends mouse click co-ordinates, show menu at that point.
@@ -43,6 +63,13 @@ ipcMain.on(`display-app-menu`, function(e, args) {
   if (isWindows && mainWindow) {
     menu.popup({
       window: mainWindow,
+      x: args.x,
+      y: args.y
+    });
+  }
+  if (isWindows && imputerWindow) {
+    menu.popup({
+      window: mainWimputerWindowindow,
       x: args.x,
       y: args.y
     });
